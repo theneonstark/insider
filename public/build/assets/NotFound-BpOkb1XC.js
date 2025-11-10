5c8225a9058e16d591d353b539397e4ebf84a19a
theneonstark <mohd47149@gmail.com>
2025-11-10 15:59:31 +0530

    build update

--- public/build/assets/NotFound-BpOkb1XC.js
+++ public/build/assets/NotFound-BpOkb1XC.js
@@ -1,4 +1,4 @@
-import{r as o,j as T}from"./app-B24lYIGE.js";/**
+import{r as o,j as T}from"./app-SQBbOcLR.js";/**
  * react-router v7.9.4
  *
  * Copyright (c) Remix Software Inc.
---
+++ public/build/assets/badge-CEe_JK0c.js
@@ -0,0 +1,6 @@
+import{c as o}from"./createLucideIcon-CEetH4QU.js";import{j as n}from"./app-SQBbOcLR.js";import{c as a,i as s}from"./index-16-0nrMD.js";/**
+ * @license lucide-react v0.548.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0",key:"1nclc0"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]],g=o("eye",i),c=s("inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",{variants:{variant:{default:"border-transparent bg-primary text-primary-foreground hover:bg-primary/80",secondary:"border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",destructive:"border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",outline:"text-foreground"}},defaultVariants:{variant:"default"}});function l({className:r,variant:e,...t}){return n.jsx("div",{className:a(c({variant:e}),r),...t})}export{l as B,g as E};
